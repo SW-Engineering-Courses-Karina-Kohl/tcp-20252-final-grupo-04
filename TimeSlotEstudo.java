c5b659be87c9ee9214e68911265226b8ac67fc5b
Pedro <pedro.schuck80@gmail.com>
2025-10-30 15:52:10 -0300

    Algumas classes adicionadas
    Classes Aluno, ConfiguracaoAgenda e TimeSlotEstudo adicionadas.

---
+++ TimeSlotEstudo.java
@@ -0,0 +1,84 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class TimeSlotEstudo {
+    public static final Duration DURACAO_PADRAO = Duration.ofMinutes(30);
+    private final LocalDateTime inicioEstudo;
+    private final Atividade atividade;
+    private final TimeSlot slotOrigem;
+    private boolean disponivel;
+
+    // Construtores
+
+    public TimeSlotEstudo(LocalDateTime inicioEstudo, Atividade atividade, TimeSlot slotOrigem) {
+        this.inicioEstudo = inicioEstudo;
+        this.atividade = atividade;
+        this.slotOrigem = slotOrigem;
+        this.disponivel = false;
+    }
+
+    public TimeSlotEstudo(LocalDateTime inicioEstudo, TimeSlot slotOrigem) {
+        this.inicioEstudo = inicioEstudo;
+        this.atividade = null;
+        this.slotOrigem = slotOrigem;
+        this.disponivel = true;
+    }
+
+    public TimeSlotEstudo(LocalDate data, LocalTime hora) {
+        this.inicioEstudo = LocalDateTime.of(data, hora);
+        this.atividade = null;
+        this.slotOrigem = null;
+        this.disponivel = true;
+    }
+
+    public TimeSlotEstudo(LocalDate data, TimeSlot timeSlot) {
+        this.inicioEstudo = LocalDateTime.of(data, timeSlot.getHoraInicio());
+        this.atividade = null;
+        this.slotOrigem = timeSlot;
+        this.disponivel = true;
+    }
+
+    // Getters e Setters
+
+    public LocalDateTime getInicioEstudo() {
+        return inicioEstudo;
+    }
+
+    public Atividade getAtividade() {
+        return atividade;
+    }
+
+    public LocalDate getData() {
+        return inicioEstudo.toLocalDate();
+    }
+
+    public LocalTime getHoraInicio() {
+        return inicioEstudo.toLocalTime();
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void setAtividade(Atividade atividade) {
+         this.atividade = atividade; 
+    }
+
+    // Outros métodos
+
+    public boolean isAtividadeValida(Atividade atividade) {
+        return atividade != null;
+    }
+
+    // tem uma função parecida em impedimento, rever depois
+    public boolean conflitaComImpedimento(Impedimento impedimento) {
+        return inicioEstudo.isEqual(impedimento.getDataHora());
+    }
+
+}
