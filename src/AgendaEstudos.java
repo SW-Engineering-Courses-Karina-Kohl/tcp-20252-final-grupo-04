74e9bf2178eca6d32a07fb2ce08594e85873b700
FilipeMoura6416 <lfsm.office@gmail.com>
2025-11-04 21:59:10 -0300

    Add files via upload

---
+++ src/AgendaEstudos.java
@@ -0,0 +1,17 @@
+import java.util.List;
+public class AgendaEstudos 
+{
+    private List<TimeSlotEstudo> estudos;
+    private boolean cronogramaCriado;
+
+    public AgendaEstudos()
+    {
+        this.cronogramaCriado = false;
+    }
+
+    public void addTimeSlotEstudo(TimeSlotEstudo timeSlotEstudo)
+    {
+        this.estudos.add(timeSlotEstudo);
+    }
+
+}
