d73004871c556463bee480c77a0298026c4a2a44
FilipeMoura6416 <lfsm.office@gmail.com>
2025-11-04 21:40:15 -0300

    Add files via upload

---
+++ src/DistribuidorAtividades.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class DistribuidorAtividades 
+{
+    public void distribuir(List<AlocacaoAtividade> alocacoes, List<TimeSlotEstudo> timeSlotsDisponiveis)
+    {
+        // Implementar lógica de distribuição dos TimeSlotEstudo para as atividades conforme a quantidade arredondada
+    }
+
+}
