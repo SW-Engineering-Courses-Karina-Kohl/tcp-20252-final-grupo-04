66d9304ffa6a93e653823032a71276d7bcbda9c1
Pedro <pedro.schuck80@gmail.com>
2025-11-06 16:00:49 -0300

    Atributo slotOrigem removido

--- src/TimeSlotEstudo.java
+++ src/TimeSlotEstudo.java
@@ -1,4 +1,3 @@
-import java.sql.Time;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalDate;
@@ -8,36 +7,25 @@ public class TimeSlotEstudo {
     public static final Duration DURACAO_PADRAO = Duration.ofMinutes(30);
     private final LocalDateTime inicioEstudo;
     private final Atividade atividade;
-    private final TimeSlot slotOrigem;
     private boolean disponivel;
 
     // Construtores
 
-    public TimeSlotEstudo(LocalDateTime inicioEstudo, Atividade atividade, TimeSlot slotOrigem) {
+    public TimeSlotEstudo(LocalDateTime inicioEstudo, Atividade atividade) {
         this.inicioEstudo = inicioEstudo;
         this.atividade = atividade;
-        this.slotOrigem = slotOrigem;
         this.disponivel = false;
     }
 
-    public TimeSlotEstudo(LocalDateTime inicioEstudo, TimeSlot slotOrigem) {
+    public TimeSlotEstudo(LocalDateTime inicioEstudo) {
         this.inicioEstudo = inicioEstudo;
         this.atividade = null;
-        this.slotOrigem = slotOrigem;
         this.disponivel = true;
     }
 
     public TimeSlotEstudo(LocalDate data, LocalTime hora) {
         this.inicioEstudo = LocalDateTime.of(data, hora);
         this.atividade = null;
-        this.slotOrigem = null;
-        this.disponivel = true;
-    }
-
-    public TimeSlotEstudo(LocalDate data, TimeSlot timeSlot) {
-        this.inicioEstudo = LocalDateTime.of(data, timeSlot.getHoraInicio());
-        this.atividade = null;
-        this.slotOrigem = timeSlot;
         this.disponivel = true;
     }
 
@@ -63,10 +51,6 @@ public class TimeSlotEstudo {
         return disponivel;
     }
 
-    public TimeSlot getSlotOrigem() {
-        return slotOrigem;
-    }
-
     public Duration getDuracao() {
         return DURACAO_PADRAO;
     }
@@ -77,10 +61,6 @@ public class TimeSlotEstudo {
 
     public void setAtividade(Atividade atividade) {
          this.atividade = atividade; 
-    }
-
-    public void setSlotOrigem(TimeSlot slotOrigem) {
-        this.slotOrigem = slotOrigem;
     }
 
     public void setInicioEstudo(LocalDateTime inicioEstudo) {
