9aec6be84cfe4c3ec7818127ac75227079dfed2d
Cristopher dos Santos <crissfilho@gmail.com>
2025-11-09 17:06:22 -0300

    esqueletos de  DiaSemana e Impedimento feitos

---
+++ DiaSemana.java
@@ -0,0 +1,28 @@
+package classes;
+
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.*;
+
+public class DiaSemana 
+{
+    private DayOfWeek diaSemana;
+    private List<LocalTime> horarios;
+
+    public DiaSemana(DayOfWeek diaSemana)
+    {
+        this.diaSemana = diaSemana;
+        this.horarios = new ArrayList<>();
+    }
+
+    public void adicionarTimeSlot(LocalTime timeSlot)
+    {
+        this.horarios.add(timeSlot);
+    }
+
+    public List<LocalTime> getHorarios()
+    {
+        return this.horarios;
+    }
+}
