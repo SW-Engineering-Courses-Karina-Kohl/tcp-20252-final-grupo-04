50a3c656c8ee01a04d45ec85e6859ba12ac9e67c
Filipe Moura <lfsm.office@gmail.com>
2025-11-14 20:01:27 -0300

    Atualização de classes e métodos

--- src/DistribuidorAtividades.java
+++ src/DistribuidorAtividades.java
@@ -6,6 +6,25 @@ public class DistribuidorAtividades
     public void distribuir(List<AlocacaoAtividade> alocacoes, List<TimeSlotEstudo> timeSlotsDisponiveis)
     {
         // Implementar lógica de distribuição dos TimeSlotEstudo para as atividades conforme a quantidade arredondada
+        // Enquanto houver timeSlotsDisponiveis e alocacoes com quantidade > 0
+        Iterator<TimeSlotEstudo> iteratorTimeSlots = timeSlotsDisponiveis.iterator();
+        int i = 0;
+        while(iteratorTimeSlots.hasNext())
+        {
+            TimeSlotEstudo timeSlot = iteratorTimeSlots.next();
+            while (!(alocacoes.get(i).getQuantidadeTimeSlotEstudosArredondada() > 0)) 
+            {
+                i = i + 1 < alocacoes.size() ? i + 1 : 0;
+                
+            }
+            AlocacaoAtividade alocacaoAtual = alocacoes.get(i);
+            // Atribuir atividade ao timeSlot
+            timeSlot.setAtividade(alocacaoAtual.getAtividade());
+            // Decrementar quantidade de timeSlots a serem atribuídos para a atividade
+            alocacaoAtual.decrementarQuantidadeTimeSlotEstudos();
+            i = i + 1 < alocacoes.size() ? i + 1 : 0;
+                
+        }
     }
 
 }
