e343091c956240b985ae5d127e68c81c6d37f97b
Pedro <pedro.schuck80@gmail.com>
2025-11-15 17:33:49 -0300

    Classe refatorada
    RegisterWeekScreen refatorada para possuir apenas atributos necessários e métodos que agrupem inicializações

--- src/RegisterWeekScreen.java
+++ src/RegisterWeekScreen.java
@@ -3,157 +3,139 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import java.awt.Font;
+import java.awt.CardLayout;
 
 public class RegisterWeekScreen {
-    private int larguraPainelRegistroSemana = 900;
-    private int alturaPainelRegistroSemana = 500;
     private JPanel registerWeekPanel;
-    private int posicaoXInfoDataInicio = 50;
-    private int posicaoYInfoDataInicio = 50;
-    private int larguraInfoDataInicio = 300;
-    private int alturaInfoDataInicio = 30;
     private JLabel infoDataInicio;
     private JTextArea dataInicioInput;
-    private int posicaoXInfoDataFim = 50;
-    private int posicaoYInfoDataFim = 100;
-    private int larguraInfoDataFim = 300;
-    private int alturaInfoDataFim = 30;
     private JLabel infoDataFim;
     private JTextArea dataFimInput;
-    private int posicaoXInfoImpedimentos = 50;
-    private int posicaoYInfoImpedimentos = 150;
-    private int larguraInfoImpedimentos = 300;
-    private int alturaInfoImpedimentos = 30;
     private JLabel infoImpedimentos;
     private JTextArea impedimentosInput;
-    private JButton segundaFeiraButton;
-    private JButton tercaFeiraButton;
-    private JButton quartaFeiraButton;
-    private JButton quintaFeiraButton;
-    private JButton sextaFeiraButton;
-    private JButton sabadoButton;
-    private JButton domingoButton;
+    private JButton[] botoesDiaSemana;
     private JButton nextScreen;
-    //métodos tela de registro de semana
-    public JButton getSegundaFeiraButton() {
-        return segundaFeiraButton;
-    }
-    public void setSegundaFeiraButton(int posicaoX, int posicaoY, int largura, int altura) {
-        this.segundaFeiraButton = new JButton("Segunda-feira");
-        this.segundaFeiraButton.setSize(largura, altura);
-        this.segundaFeiraButton.setLocation(posicaoX, posicaoY);
-    }
-    public JButton getTercaFeiraButton() {
-        return tercaFeiraButton;
-    }
-    public void setTercaFeiraButton(int posicaoX, int posicaoY, int largura, int altura) {
-        this.tercaFeiraButton = new JButton("Terça-feira");
-        this.tercaFeiraButton.setSize(largura, altura);
-        this.tercaFeiraButton.setLocation(posicaoX, posicaoY);
-    }
-    public JButton getQuartaFeiraButton() {
-        return quartaFeiraButton;
-    }
-    public void setQuartaFeiraButton(int posicaoX, int posicaoY, int largura, int altura) {
-        this.quartaFeiraButton = new JButton("Quarta-feira");
-        this.quartaFeiraButton.setSize(largura, altura);
-        this.quartaFeiraButton.setLocation(posicaoX, posicaoY);
-    }
-    public JButton getQuintaFeiraButton() {
-        return quintaFeiraButton;
-    }
-    public void setQuintaFeiraButton(int posicaoX, int posicaoY, int largura, int altura) {
-        this.quintaFeiraButton = new JButton("Quinta-feira");
-        this.quintaFeiraButton.setSize(largura, altura);
-        this.quintaFeiraButton.setLocation(posicaoX, posicaoY);
-    }
-    public JButton getSextaFeiraButton() {
-        return sextaFeiraButton;
-    }
-    public void setSextaFeiraButton(int posicaoX, int posicaoY, int largura, int altura) {
-        this.sextaFeiraButton = new JButton("Sexta-feira");
-        this.sextaFeiraButton.setSize(largura, altura);
-        this.sextaFeiraButton.setLocation(posicaoX, posicaoY);
-    }
-    public JButton getSabadoButton() {
-        return sabadoButton;
-    }
-    public void setSabadoButton(int posicaoX, int posicaoY, int largura, int altura) {
-        this.sabadoButton = new JButton("Sábado");
-        this.sabadoButton.setSize(largura, altura);
-        this.sabadoButton.setLocation(posicaoX, posicaoY);
-    }
-    public JButton getDomingoButton() {
-        return domingoButton;
-    }
-    public void setDomingoButton(int posicaoX, int posicaoY, int largura, int altura) {
-        this.domingoButton = new JButton("Domingo");
-        this.domingoButton.setSize(largura, altura);
-        this.domingoButton.setLocation(posicaoX, posicaoY);
-    }
+    private String dataInicio;
+    private String dataFim;
+    private String impedimentos;
+   
     public JButton getNextScreenButton() {
         return nextScreen;
     }
-    public void setNextScreenButton(int posicaoX, int posicaoY, int largura, int altura) {
+    public void setNextScreenButton() {
         this.nextScreen = new JButton("Próxima Tela");
-        this.nextScreen.setSize(largura, altura);
-        this.nextScreen.setLocation(posicaoX, posicaoY);
+        this.nextScreen.setSize(200, 50);
+        this.nextScreen.setLocation(670, 390);
         this.nextScreen.setFont(new Font("Arial", Font.PLAIN, 20));
     }
     public JPanel getRegisterWeekPanel() {
         return registerWeekPanel;
     }
-    public void setRegisterWeekPanel(int largura, int altura) {
+    public void setRegisterWeekPanel() {
         this.registerWeekPanel = new JPanel();
         this.registerWeekPanel.setLayout(null);
-        this.registerWeekPanel.setSize(largura, altura);
+        this.registerWeekPanel.setSize(900, 500);
     }
     public JLabel getInfoDataInicioLabel() {
         return infoDataInicio;
     }
-    public void setInfoDataInicioLabel(int posicaoX, int posicaoY, int largura, int altura) {
+    public void setInfoDataInicioLabel() {
         this.infoDataInicio = new JLabel("Data Início Vigência (AAAA-MM-DD):");
-        this.infoDataInicio.setSize(largura, altura);
-        this.infoDataInicio.setLocation(posicaoX, posicaoY);
+        this.infoDataInicio.setSize(300, 30);
+        this.infoDataInicio.setLocation(50, 50);
     }
     public JTextArea getDataInicioInput() {
         return dataInicioInput;
     }
-    public void setDataInicioInput(int posicaoX, int posicaoY, int largura, int altura) {
+    public void setDataInicioInput() {
         this.dataInicioInput = new JTextArea();
-        this.dataInicioInput.setSize(largura, altura);
-        this.dataInicioInput.setLocation(posicaoX, posicaoY);
+        this.dataInicioInput.setSize(200, 30);
+        this.dataInicioInput.setLocation(320, 50);
     }
     public JLabel getInfoDataFimLabel() {
         return infoDataFim;
     }
-    public void setInfoDataFimLabel(int posicaoX, int posicaoY, int largura, int altura) {
+    public void setInfoDataFimLabel() {
         this.infoDataFim = new JLabel("Data Fim Vigência (AAAA-MM-DD):");
-        this.infoDataFim.setSize(largura, altura);
-        this.infoDataFim.setLocation(posicaoX, posicaoY);
+        this.infoDataFim.setSize(300, 30);
+        this.infoDataFim.setLocation(50, 100);
     }
     public JTextArea getDataFimInput() {
         return dataFimInput;
     }
-    public void setDataFimInput(int posicaoX, int posicaoY, int largura, int altura) {
+    public void setDataFimInput() {
         this.dataFimInput = new JTextArea();
-        this.dataFimInput.setSize(largura, altura);
-        this.dataFimInput.setLocation(posicaoX, posicaoY);
+        this.dataFimInput.setSize(200, 30);
+        this.dataFimInput.setLocation(320, 100);
     }
     public JLabel getInfoImpedimentosLabel() {
         return infoImpedimentos;
     }
-    public void setInfoImpedimentosLabel(int posicaoX, int posicaoY, int largura, int altura) {
+    public void setInfoImpedimentosLabel() {
         this.infoImpedimentos = new JLabel("Impedimentos (AAAA-MM-DD HH:MM):");
-        this.infoImpedimentos.setSize(largura, altura);
-        this.infoImpedimentos.setLocation(posicaoX, posicaoY);
+        this.infoImpedimentos.setSize(300, 30);
+        this.infoImpedimentos.setLocation(50, 150);
     }
     public JTextArea getImpedimentosInput() {
         return impedimentosInput;
     }
-    public void setImpedimentosInput(int posicaoX, int posicaoY, int largura, int altura) {
+    public void setImpedimentosInput() {
         this.impedimentosInput = new JTextArea();
-        this.impedimentosInput.setSize(largura, altura);
-        this.impedimentosInput.setLocation(posicaoX, posicaoY);
+        this.impedimentosInput.setSize(200, 30);
+        this.impedimentosInput.setLocation(320, 150);
+    }
+    public void setWeekDaysButtons(JPanel panel, CardLayout cardLayout) {
+        String[] diasSemana = {"Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"};
+        botoesDiaSemana = new JButton[diasSemana.length];
+        for (int i = 0; i < diasSemana.length; i++) {
+            botoesDiaSemana[i] = new JButton(diasSemana[i]);
+            int botoesIndex = i;
+            botoesDiaSemana[i].addActionListener(e -> {
+                cardLayout.show(panel, "Register" + botoesIndex + "TimeSlotPanel");
+            });
+            botoesDiaSemana[i].setSize(150, 50);
+            botoesDiaSemana[i].setLocation(50 + (i % 4) * 200, 250 + (i / 4) * 70);
+            this.registerWeekPanel.add(botoesDiaSemana[i]);
+        }
+    }
+    public void transitionToCalendarScreen(JPanel panel, CardLayout cardLayout) {
+        nextScreen.addActionListener(e -> {
+            dataInicio = dataInicioInput.getText();
+            dataFim = dataFimInput.getText();
+            impedimentos = impedimentosInput.getText();
+            // Temporário, apenas para verificação no console
+            System.out.println("Data Início: " + dataInicio);
+            System.out.println("Data Fim: " + dataFim);
+            System.out.println("Impedimentos: " + impedimentos);
+            cardLayout.show(panel, "CalendarPanel");
+        });
+    }
+    public String getDataInicio() {
+        return dataInicio;
+    }
+    public String getDataFim() {
+        return dataFim;
+    }
+    public String getImpedimentos() {
+        return impedimentos;
+    }
+    public void initializeRegisterWeekScreen(RegisterWeekScreen rws, JPanel panel, CardLayout cardLayout) {
+        rws.setRegisterWeekPanel();
+        rws.setNextScreenButton();
+        rws.getRegisterWeekPanel().add(rws.getNextScreenButton());
+        rws.setInfoDataInicioLabel();
+        rws.setDataInicioInput();
+        rws.getRegisterWeekPanel().add(rws.getDataInicioInput());
+        rws.getRegisterWeekPanel().add(rws.getInfoDataInicioLabel());
+        rws.setInfoDataFimLabel();
+        rws.setDataFimInput();
+        rws.getRegisterWeekPanel().add(rws.getDataFimInput());
+        rws.getRegisterWeekPanel().add(rws.getInfoDataFimLabel());
+        rws.setInfoImpedimentosLabel();
+        rws.getRegisterWeekPanel().add(rws.getInfoImpedimentosLabel());
+        rws.setImpedimentosInput();
+        rws.getRegisterWeekPanel().add(rws.getImpedimentosInput());
+        rws.setWeekDaysButtons(panel, cardLayout);
+        rws.transitionToCalendarScreen(panel, cardLayout);
     }
 }
