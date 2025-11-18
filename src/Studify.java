9fe7ac1ed3150f0e1e1bc485776944e2ff992af8
Pedro <pedro.schuck80@gmail.com>
2025-11-18 17:11:49 -0300

    Refatoração de nomes
    Refatoração para mudar nome de classes e variáveis para português.

--- src/Studify.java
+++ src/Studify.java
@@ -4,33 +4,32 @@ import java.awt.CardLayout;
 
 public class Studify {
     public static void main(String[] args) {
-        System.out.println("Welcome to Studify!");
-        JFrame frame = new JFrame("Studify");
-        frame.setSize(900, 500);
-        JPanel panel = new JPanel();
+        JFrame janela = new JFrame("Studify");
+        janela.setSize(900, 500);
+        JPanel painel = new JPanel();
         CardLayout cardLayout = new CardLayout();
-        panel.setLayout(cardLayout);
-        TitleScreen ts = new TitleScreen();
-        ts.initializeTitleScreen(ts);
-        ts.transitionToRegisterWeekScreen(panel, cardLayout);
-        panel.add(ts.getTitlePanel(), "TitlePanel");
+        painel.setLayout(cardLayout);
+        TelaInicial telaInicial = new TelaInicial();
+        telaInicial.inicializaTelaInicial();
+        telaInicial.transicaoParaTelaRegistrarSemana(painel, cardLayout);
+        painel.add(telaInicial.getPainelInicial(), "PainelInicial");
         //segunda tela
-        RegisterWeekScreen rws = new RegisterWeekScreen();
-        rws.initializeRegisterWeekScreen(rws, panel, cardLayout);
-        panel.add(rws.getRegisterWeekPanel(), "RegisterWeekPanel");
+        TelaRegistrarSemana telaRegistrarSemana = new TelaRegistrarSemana();
+        telaRegistrarSemana.inicializaTelaRegistrarSemana(painel, cardLayout);
+        painel.add(telaRegistrarSemana.getPainelRegistrarSemana(), "PainelRegistrarSemana");
         //temporário enquanto não faz a tela de calendário e dos time slots
-        JPanel calendarPanel = new JPanel();
-        calendarPanel.setSize(900, 500);
+        JPanel painelAgenda = new JPanel();
+        painelAgenda.setSize(900, 500);
         for (int i = 0; i < 7; i++) {
-            JPanel[] dayPanels = new JPanel[7];
-            dayPanels[i] = new JPanel();
-            dayPanels[i].setSize(900, 500);
-            dayPanels[i].setLayout(null);
-            panel.add(dayPanels[i], "Register" + i + "TimeSlotPanel");
+            JPanel[] paineisDiasSemana = new JPanel[7];
+            paineisDiasSemana[i] = new JPanel();
+            paineisDiasSemana[i].setSize(900, 500);
+            paineisDiasSemana[i].setLayout(null);
+            painel.add(paineisDiasSemana[i], "Registrar" + i + "PainelTimeSlot");
         }
-        panel.add(calendarPanel, "CalendarPanel");
-        frame.add(panel);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        painel.add(painelAgenda, "PainelAgenda");
+        janela.add(painel);
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
